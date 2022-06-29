be5eeb3c2c3838c4ec4454cda3fbacf6dea9f4a9
Robin Ward <robin.ward@gmail.com>
2022-06-29 16:34:31 -0400

    Has a PC

--- project/main.gd
+++ project/main.gd
@@ -1,9 +1,10 @@
 extends Spatial
 
-
-onready var material: SpatialMaterial = $CSGBox.material
+onready var pc = $PC
+onready var screen = $PC/screen
 var image: Image = null
 var image_tex: ImageTexture = null
+var material: Material = null
 
 # load the SIMPLE library
 const SARIEN = preload("res://gdnative/sarien.gdns")
@@ -13,6 +14,7 @@ func _ready():
 	image = Image.new()
 	image.create_from_data(320, 200, false, Image.FORMAT_RGB8, sarien.get_frame())
 
+	material = screen.get_surface_material(0)
 	image_tex = ImageTexture.new()
 	image_tex.create_from_image(image, 0)
 
@@ -22,6 +24,6 @@ func update_texture():
 	image.create_from_data(320, 200, false, Image.FORMAT_RGB8, sarien.get_frame())
 	image_tex.set_data(image)
 
-func _process(_delta: float):
-#	$CSGBox.rotate_z(delta)
+func _process(delta: float):
+#	pc.rotate_y(delta * 0.1)
 	update_texture()
